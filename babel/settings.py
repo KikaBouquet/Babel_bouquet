6f0b04b1fb9e34c8dc8ad6b2763f76b564ccc73e
Kika <projet.jessicabouquet@gmail.com>
2020-01-22 11:23:48 +0100

    change settings.py for heroku

--- babel/settings.py
+++ babel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +121,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "files_static")
 STATIC_MEDIA = os.path.join(BASE_DIR, "files_media")
 print(f"STATIC_ROOT = {STATIC_ROOT}")
 print(f"STATIC_MEDIA = {STATIC_MEDIA}")
+
+django_heroku.settings(locals())
